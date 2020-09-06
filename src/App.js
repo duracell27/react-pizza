3bd2ef92b32d268563bca708177a6be8683aa4a6
duracell27 <volodymyrshmidt@gmail.com>
2020-09-06 21:25:11 +0300

    5 lesson ready

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {Header} from './Components';
 import {Home, Cart} from './Pages'
 import { Route } from 'react-router-dom';
+import Axios from 'axios';
+
 
 
 function App() {
+
+  const [pizzas, setPizzas] = React.useState([])
+
+  useEffect(()=>{
+    Axios.get('http://localhost:3000/db.json').then(({data})=>{
+      setPizzas(data.pizzas)
+    })
+  }, [])
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path='/' component={Home} />
+        <Route exact path='/' render={()=> <Home pizzas={pizzas}/>} />
         <Route exact path='/cart' component={Cart} />
       </div>
     </div>
