bfa6e2ca3a49d7784858d7c9b819833768bcadc0
duracell27 <volodymyrshmidt@gmail.com>
2020-09-10 00:06:54 +0300

    8 lesson 1 part

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import React, { useEffect } from 'react';
 import { Header } from './Components';
 import { Home, Cart } from './Pages'
 import { Route } from 'react-router-dom';
-import Axios from 'axios';
 import { useDispatch } from 'react-redux';
-import { setPizzas } from './redux/actions/pizzas'
+import { fetchPizzas } from './redux/actions/pizzas'
 
 function App() {
 
   const dispatch = useDispatch()
 
   useEffect(() => {
-    Axios.get('http://localhost:3001/pizzas?_sort=price&_order=desc').then(({ data }) => {
-      dispatch(setPizzas(data))
-    })
+    dispatch(fetchPizzas())
   }, [])
 
   return (
